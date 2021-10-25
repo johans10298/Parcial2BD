c013749ce991457d76e84a0dc1f830247d00917d
= <=>
2021-10-24 22:22:07 -0500

    Mi mensaje super claro para los otros programadores

---
+++ app.py
@@ -0,0 +1,204 @@
+import json 
+import boto3
+import requests
+import datetime
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+s3= boto3.client('s3')
+def handler(event,context):
+    r = requests.get("https://www.eltiempo.com")
+    e = requests.get("https://www.elespectador.com")
+    x=r.text
+    e=e.text
+    archivo = open("/tmp/archivo.txt","w")
+    archivo.write(str(x))
+    archivo.close()
+    archivo1 = open("/tmp/archivo1.txt","w")
+    archivo1.write(str(e))
+    archivo1.close()
+    date = datetime.datetime.now()
+    year= date.year
+    month= date.month
+    day = date.day
+    url = "Headlines/Raw/periodico=El Tiempo""/year="+str(year)+"/month="+str(month)+"/day="+str(day)+"/tiempo.txt"
+    url1 = "Headlines/Raw/periodico=El Espectador""/year="+str(year)+"/month="+str(month)+"/day="+str(day)+"/espectador.txt"
+    s3.upload_file("/tmp/archivo.txt","parcial2-1",url)
+    s3.upload_file("/tmp/archivo1.txt","parcial2-1",url1)
+    return {'statusCode' : 200}
+
+def scrap(event,context):
+    date = datetime.datetime.now()
+    year= date.year
+    month= date.month
+    day = date.day
+    url = "Headlines/Raw/periodico=El Tiempo""/year="+str(year)+"/month="+str(month)+"/day="+str(day)+"/tiempo.txt"
+    url1 = "Headlines/Raw/periodico=El Espectador""/year="+str(year)+"/month="+str(month)+"/day="+str(day)+"/espectador.txt"
+    response1 = s3.get_object(Bucket="parcial2-1",Key=url)
+    data1 = response1['Body'].read()
+    print("hola", data1)
+    response = s3.get_object(Bucket="parcial2-1",Key=url)
+    data = response['Body'].read()
+    print("hola", data)
+
+    e = requests.get('https://www.eltiempo.com').text
+    #print(e)
+
+    soup = BeautifulSoup(data1,'html.parser')
+    link= list()
+    titulo= list()
+    categoria=list()
+    categoria1=list()
+    for a in soup.find_all('a',class_="title page-link",href=True):
+        categoria.append(str(a['href']))
+        link.append("https://www.eltiempo.com"+str(a['href']))
+
+    for a in soup.find_all('a',class_="title page-link"):
+        titulo.append(str(a.text))
+
+    for a in range(len(categoria)):
+        categoria1.append(categoria[a].split(sep='/')[1])
+
+    print(len(titulo))
+    for a in range(len(titulo)):
+        print(titulo[a])
+        print(link[a])
+        print(categoria1[a])
+
+    dict1= {'titulo': titulo, 'categoria':categoria1,'link':link}
+
+    url = 'https://www.elespectador.com/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    titulo = []
+
+    url = 'https://www.elespectador.com/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    titulo = []
+
+    et =  soup.find_all('h2', class_ = 'Card-Title Title Title')
+    #Titulos de los articulos
+    for i in soup.find_all('h2', class_ = 'Card-Title Title Title'):
+        titulo.append(i.text)
+
+    for i in titulo:
+        print(i)
+
+    array = []
+    soupS = str(soup)
+    vali1 = 82
+    valf1 = 86
+    lim1 = 188
+
+    vali2 = 112
+    valf2 = 116
+    lim2 = 215
+
+    vali3 = 108
+    valf3 = 112
+    lim3 = 218
+
+    vali4 = 4737
+    valf4 = 4741
+    lim4 = 4835
+
+    vali5 = 4767
+    valf5 = 4771
+    lim5 = 4840
+
+    #print(soupS)
+    for i in soup.find_all('h2', class_ = 'Card-Title Title Title'):
+        if str(i)[vali1:valf1] == 'href':
+            array.append('https://www.elespectador.com'+str(i)[vali1+6:lim1])
+        elif str(i)[vali2:valf2] == 'href':
+         array.append('https://www.elespectador.com'+str(i)[vali2+6:lim2])
+        elif str(i)[vali3:valf3] == 'href':
+            array.append('https://www.elespectador.com'+str(i)[vali3+6:lim3])
+        elif str(i)[vali4:valf4] == 'href':
+         array.append('https://www.elespectador.com'+str(i)[vali4+6:lim4])
+        elif str(i)[vali5:valf5] == 'href':
+            array.append('https://www.elespectador.com'+str(i)[vali5+6:lim5])
+   
+
+    string = 'rel="noreferrer" target="_self">En Unió'
+    txt =''
+    bool = False
+    link = []
+    for i in array:
+        var = i[::-1]
+        for j in i[::-1]:
+            if j == '/':
+             bool = True
+            if bool == True:
+                txt = txt+j
+    #print(txt[::-1])
+        link.append(txt[::-1])
+        bool = False
+        txt = ''
+
+    for i in link:
+        print(i)
+
+    cat = []
+    for i in array:
+        if i.find('mundo') > 0:
+            cat.append('mundo')
+        elif i.find('economia')  > 0 :
+            cat.append('economia')
+        elif i.find('el-magazin-cultural') > 0 :
+            cat.append('el-magazin-cultural')
+        elif i.find('entretenimiento') > 0 :
+            cat.append('entretenimiento')
+        elif i.find('opinion') > 0 :
+            cat.append('opinion')
+        elif i.find('colombia') > 0 :
+            cat.append('colombia')
+        elif i.find('salud') > 0 :
+            cat.append('salud')
+        elif i.find('contenido-patrocinado') > 0:
+            cat.append('contenido-patrocinado')
+        elif i.find('judicial') > 0:
+            cat.append('judicial')
+        elif i.find('bogota') > 0:
+            cat.append('bogota')
+        elif i.find('ciencia') > 0:
+            cat.append('ciencia')
+        elif i.find('ambiente') > 0:
+            cat.append('ambiente')
+
+    for i in cat:
+        print(i)
+    dict= {'titulo':titulo, 'categoria':cat,'link':link}
+
+    print(len(titulo))
+    print(len(link))
+    print(len(cat))
+
+    df = pd.DataFrame(dict) 
+    df.to_csv('/tmp/espectador.csv')
+
+    for i in cat:
+     print(i)
+
+    df1 = pd.DataFrame(dict1) 
+    df1.to_csv('/tmp/tiempo.csv')
+    url11 = "Headlines/news/periodico=El Tiempo""/year="+str(year)+"/month="+str(month)+"/day="+str(day)+"/Eltiempo.csv"
+    s3.upload_file("/tmp/tiempo.csv","parcial2-2",url11)
+    url12 = "Headlines/news/periodico=El Espectador""/year="+str(year)+"/month="+str(month)+"/day="+str(day)+"/espectador.csv"
+    s3.upload_file("/tmp/espectador.csv","parcial2-2",url12)
+
+def lambda_handler(event, context):
+    bucket_name = 'parcial-2'
+    client = boto3.client('athena')
+    config = {
+        'OutputLocation': 's3://' + bucket_name + '/',
+        'EncryptionConfiguration': {'EncryptionOption': 'SSE_S3'}
+    }
+    # Query Execution Parameters
+    sql = 'MSCK REPAIR TABLE news'
+    context = {'Database': 'parcial'}
+
+    client.start_query_execution(QueryString = sql, 
+                                 QueryExecutionContext = context,
+                                 ResultConfiguration = config)
